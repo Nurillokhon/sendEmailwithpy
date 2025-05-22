af2e91a1eb7f5b10ff582450defa5d868442cb28
Husan Ibragimov <husandev@mail.ru>
2025-05-22 17:12:49 +0500

    feat: add email field to OrderData schema and remove product description

--- src/app/schema.py
+++ src/app/schema.py
@@ -6,6 +6,7 @@ from pydantic import BaseModel, EmailStr
 
 class OrderData(BaseModel):
     order_number: str
+    email: EmailStr
     first_name: str
     last_name: str
     address: str
@@ -13,14 +14,11 @@ class OrderData(BaseModel):
     city: str
     country: str
     phone: str
-    email: EmailStr
     product_name: str
     product_price: str
-    product_describ: str = None
     product_image: str = None
     passImage: str = None
     grand_total: str
-
 
 async def get_order_data(
         first_name: str = Form(...),
@@ -33,7 +31,6 @@ async def get_order_data(
         email: str = Form(...),
         product_name: str = Form(...),
         product_price: str = Form(...),
-        product_describ: Optional[str] = Form(None),
         grand_total: str = Form(...),
         product_image: Optional[UploadFile] = File(None),
         passImage: Optional[UploadFile] = File(None),
@@ -49,7 +46,6 @@ async def get_order_data(
         "email": email,
         "product_name": product_name,
         "product_price": product_price,
-        "product_describ": product_describ,
         "grand_total": grand_total,
         "product_image": product_image,
         "passImage": passImage,
